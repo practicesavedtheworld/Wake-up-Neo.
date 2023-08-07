760dc66d666fa34cf4f926180240ae47523505c0
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-08-07 16:00:55 +0300

    add __init__

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .wake_up_neo_lib.matrix_op import Matrix2DOperations, MatrixView, Matrix2D
+
+m2d_obj = Matrix2D
+m2d_op = Matrix2DOperations
+m2d_view = MatrixView
+__all__ = ["m2d_obj", "m2d_op", "m2d_view"]
--- wake_up_neo_lib/__init__.py
+++ wake_up_neo_lib/__init__.py
@@ -1,3 +1,1 @@
-from .matrix_op import Matrix2DOperations as matrix2d_obj
 
-__all__ = ["matrix2d_obj"]
